7222aec7f436c98084ff287bd197a0b1010a585c
junyoungs7 <junyoungs7@naver.com>
2023-05-31 15:17:41 +0900

    Initial commit

---
+++ src/test/java/com/example/practicegithubActiondeploy/PracticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
